c7388fbb05ae43aa858570e1dfe792cb6d23f39d
Ekeyekwu Oscar <oscarekeyekwu@gmail.com>
2025-10-12 05:27:52 +0100

    Set up swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,6 +27,21 @@ async function bootstrap() {
 
   // Global prefix
   app.setGlobalPrefix('api');
+
+  // --- Swagger Config ---
+  const config = new DocumentBuilder()
+    .setTitle('OrbitX Dispatch API')
+    .setDescription('API documentation for the OrbitX Dispatch Application')
+    .setVersion('1.0')
+    .addBearerAuth() // Enables JWT authentication header support
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // Keep JWT auth after reload
+    },
+  });
 
   const port = (configService.get('PORT') as number) || 3000;
   await app.listen(port);
